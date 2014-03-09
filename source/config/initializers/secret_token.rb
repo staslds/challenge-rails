808e0b9dc57ae160298e323b50a2e560c03f4099
Stas <stas@lodoss.org>
2014-03-09 22:31:43 +0400

    init app

---
+++ source/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cjcoupons::Application.config.secret_key_base = '1e52a85101d91c66d07644be82a35ebc17d732380a175ea797df1e40953061d665d7bb98dd4d63d06d926c95c11ae9805fc878d003c7c5bbef879da523c93196'
